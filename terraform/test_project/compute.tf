fd7205d0f1ba7ee8b2692cb96d32e9a1d34fc709
shaun bell - 3026975 <shb00123@students.stir.ac.uk>
2025-10-24 21:19:25 +0100

    add to the user_data and Deploy via SSM using Git

--- terraform/test_project/compute.tf
+++ terraform/test_project/compute.tf
@@ -1,12 +1,10 @@
-
 data "aws_ssm_parameter" "ubuntu_24_04" {
   name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
 }
 
 data "aws_ami" "ubuntu_ami" {
-
   most_recent = true
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "image-id"
@@ -14,13 +12,10 @@ data "aws_ami" "ubuntu_ami" {
   }
 }
 
-
 resource "aws_instance" "ubuntu_instance" {
-
-  ami = data.aws_ami.ubuntu_ami.id
+  ami           = data.aws_ami.ubuntu_ami.id
   instance_type = "t3.micro"
-
-  subnet_id = aws_subnet.ssm-private-subnet.id
+  subnet_id     = aws_subnet.ssm-private-subnet.id
 
   vpc_security_group_ids = [
     aws_security_group.ssm_sg.id,
@@ -30,18 +25,20 @@ resource "aws_instance" "ubuntu_instance" {
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
 
   user_data = <<-EOF
-                #!/bin/bash
+              #!/bin/bash
+              set -e
 
-                apt update
-                snap install amazon-ssm-agent --classic
+              # Update & install SSM agent
+              apt update
+              snap install amazon-ssm-agent --classic
+              systemctl enable --now snap.amazon-ssm-agent.amazon-ssm-agent.service
 
-                systemctl enable --now snap.amazon-ssm-agent.amazon-ssm-agent.service
+              # Install Git
+              apt install -y git
 
-                apt install -y git
-
-                mkdir -p /home/ubuntu/app
-                chown ubuntu:ubuntu /home/ubuntu/app
-
+              # Create app directory with proper ownership
+              mkdir -p /home/ubuntu/app
+              chown -R ubuntu:ubuntu /home/ubuntu/app
               EOF
 
   tags = {
